86adee59880a7eba41fddf2766843d83ca7fbd93
Hamidreza <hamidrezashahnazari.99@gmail.com>
2025-10-29 22:57:41 +0330

    carbin web

---
+++ script.js
@@ -0,0 +1,9 @@
+// انیمیشن اسکرول نرم برای لینک‌های ناوبری
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
