1abda521bfe654f57f234e29aa3c2274e52fc1f8
Hamidreza <hamidrezashahnazari.99@gmail.com>
2025-11-02 22:31:58 +0330

    new desing with new them colors

--- script.js
+++
@@ -1,9 +0,0 @@
-// انیمیشن اسکرول نرم برای لینک‌های ناوبری
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', e => {
-    e.preventDefault();
-    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth'
-    });
-  });
-});
